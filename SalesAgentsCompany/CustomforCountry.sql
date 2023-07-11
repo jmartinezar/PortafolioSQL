0ee9feb9cc82d45223aa8098facda2cc337be75a
jmartinezar <jmartinezar@unal.edu.co>
2023-07-11 12:08:30 -0500

    se modificó para que cuente la cantidad de clientes por país

--- SalesAgentsCompany/CustomforCountry.sql
+++ SalesAgentsCompany/CustomforCountry.sql
@@ -1,4 +1,6 @@
-SELECT "CUST_COUNTRY", "CUST_NAME" AS "CUST_FOR_COUNTRY"
+/*El código crea una tabla de dos columnas con el nombre y el país de cada cliente de la empresa, está organizada alfabéticamente por el nombre*/
+
+SELECT "CUST_COUNTRY", COUNT("CUST_NAME") AS "CUST_FOR_COUNTRY"
        FROM "CUSTOMER"
-       GROUP BY "CUST_COUNTRY", "CUST_NAME"
-       ORDER BY 2
+       GROUP BY "CUST_COUNTRY"
+       ORDER BY 1

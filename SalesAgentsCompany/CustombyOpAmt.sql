a8f18a6272d596fdf020e94f83c084b361a9d234
jmartinezar <jmartinezar@unal.edu.co>
2023-07-11 13:01:51 -0500

    Se modifica el código para que solo muestre los tres clientes con mayor monto de operación, de mayor a menor, usando LIMIT

--- SalesAgentsCompany/CustombyOpAmt.sql
+++ SalesAgentsCompany/CustombyOpAmt.sql
@@ -1,6 +1,6 @@
-/*El presente código funciona para proporcionar una tabla de los clientes con su código, su nombre, su área de trabajo y su monto operativo, organizadas, en la tabla solo entrar los clientes para los cuales el monto de operación es superior a US$ 50 y están organizados de mayor a menor monto*/
+/*El presente código funciona para proporcionar una tabla de los clientes con su código, su nombre, su área de trabajo y su monto operativo, organizadas, en la tabla solo entran los tres clientes con mayor monto de operación, siempre que dicho monto sea superior a US$ 50*/
 
 SELECT "CUST_CODE", "CUST_NAME", "WORKING_AREA", "OPENING_AMT"
 FROM "CUSTOMER"
 WHERE "OPENING_AMT">50
-ORDER BY 4 DESC;
+ORDER BY 4 DESC LIMIT 3;

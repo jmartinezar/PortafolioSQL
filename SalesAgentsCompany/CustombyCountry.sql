8e4373b5dacc3d79e31cea8a18438c314ea2f12e
jmartinezar <jmartinezar@unal.edu.co>
2023-07-11 13:06:42 -0500

    Se agrega al archivo una descripción más precisa sobre lo que hace

--- SalesAgentsCompany/CustombyCountry.sql
+++ SalesAgentsCompany/CustombyCountry.sql
@@ -1,4 +1,4 @@
-/*El código crea una tabla de dos columnas con el nombre y el país de cada cliente de la empresa, está organizada alfabéticamente por el nombre*/
+/*El código crea una tabla de dos columnas: la primera con los nombres de los países en los que la empresa cuenta con más de dos clientes y la segunda con la cantidad de clientes que tiene la empresa en cada uno de estos países, está organizada alfabéticamente según el nombre de los países.*/
 
 SELECT "CUST_COUNTRY", COUNT("CUST_NAME") AS "CUST_FOR_COUNTRY"
        FROM "CUSTOMER"

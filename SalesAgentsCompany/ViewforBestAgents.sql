e1f16f32910f88c906ded0f89ff6fdc5e1370e49
jmartinezar <jmartinezar@unal.edu.co>
2023-07-12 11:14:14 -0500

    Create a view of a query that show the 5 best agents in orders

---
+++ SalesAgentsCompany/ViewforBestAgents.sql
@@ -0,0 +1,7 @@
+/*Este query crea una vista de los 5 agentes con mayor cantidad de ordernes realizadas*/
+
+CREATE VIEW "BEST_AGENTS" AS
+       (SELECT A."AGENT_NAME" AS "AGENT NAME", COUNT("ORD_NUM") AS "ORDERS AMOUNT"
+       FROM "AGENTS" A JOIN "ORDERS" O ON A."AGENT_CODE"=O."AGENT_CODE"
+       GROUP BY A."AGENT_NAME"
+       ORDER BY 2 DESC LIMIT 5);

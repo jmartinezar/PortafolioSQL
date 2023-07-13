4954bf90a1c01c4a491d120d66dca7a654728b5a
jmartinezar <jmartinezar@unal.edu.co>
2023-07-13 13:15:19 -0500

    se agrega un query que toma los dos querys anteriores y los usa como subquery para crear una tabla que contenga los partidos perdidos, empatados y ganados por equipo

--- SoccerDB/SoccerDataAnalysis.sql
+++ SoccerDB/SoccerDataAnalysis.sql
@@ -1,4 +1,4 @@
---Query inicial, busca en principio generar una tabla de posiciones estándar
+/*--Query inicial, busca en principio generar una tabla de posiciones estándar
 SELECT TEAM_API_ID, CASE 
 					WHEN HOME_TEAM_GOAL > AWAY_TEAM_GOAL THEN 'W' 
 					WHEN HOME_TEAM_GOAL = AWAY_TEAM_GOAL THEN 'D' 
@@ -25,3 +25,23 @@ SELECT TEAM_API_ID, COUNT(CASE WHEN home_team_goal < away_team_goal THEN 1 END) 
 FROM TEAM T JOIN MATCH M ON (T.TEAM_API_ID = M.AWAY_TEAM_API_ID)
 WHERE LEAGUE_ID = 1
 GROUP BY 1;
+*/
+SELECT L.TEAM_API_ID, 
+		SUM(L.PartidosPerdidos+V.PartidosPerdidos) AS "MATCHES WIN",
+		SUM(L.PartidosEmpatados+V.PartidosEmpatados) AS "MATCHES DRAW",
+		SUM(L.PartidosPerdidos+V.PartidosPerdidos) AS "MATCHES DRAW"
+FROM (SELECT TEAM_API_ID, COUNT(CASE WHEN home_team_goal < away_team_goal THEN 1 END) AS PartidosPerdidos,
+       COUNT(CASE WHEN home_team_goal = away_team_goal THEN 1 END) AS PartidosEmpatados,
+       COUNT(CASE WHEN home_team_goal > away_team_goal THEN 1 END) AS PartidosGanados
+		FROM TEAM T JOIN MATCH M ON (T.TEAM_API_ID = M.HOME_TEAM_API_ID)
+		WHERE LEAGUE_ID = 1
+		GROUP BY 1) L 
+	JOIN
+		(SELECT TEAM_API_ID, COUNT(CASE WHEN home_team_goal < away_team_goal THEN 1 END) AS PartidosPerdidos,
+       COUNT(CASE WHEN home_team_goal = away_team_goal THEN 1 END) AS PartidosEmpatados,
+       COUNT(CASE WHEN home_team_goal > away_team_goal THEN 1 END) AS PartidosGanados
+		FROM TEAM T JOIN MATCH M ON (T.TEAM_API_ID = M.AWAY_TEAM_API_ID)
+		WHERE LEAGUE_ID = 1
+	GROUP BY 1) V
+GROUP BY 1
+ORDER BY 4 DESC;

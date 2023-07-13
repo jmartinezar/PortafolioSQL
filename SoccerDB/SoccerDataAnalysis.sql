b197195698ca8c5a9efcb4645a19c0d01c6bf0a6
jmartinezar <jmartinezar@unal.edu.co>
2023-07-13 13:36:50 -0500

    se modificó un error en los valores de las columnas (estaban repetidas) y se agregó un condicional a ambos subqueries para que arroje solo los resultados correspondientes a una temporada en específico

--- SoccerDB/SoccerDataAnalysis.sql
+++ SoccerDB/SoccerDataAnalysis.sql
@@ -1,4 +1,5 @@
---Query inicial, busca en principio generar una tabla de posiciones estándar
+/*--Query inicial, busca en principio generar una tabla de posiciones estándar
+
 SELECT TEAM_API_ID, CASE 
 					WHEN HOME_TEAM_GOAL > AWAY_TEAM_GOAL THEN 'W' 
 					WHEN HOME_TEAM_GOAL = AWAY_TEAM_GOAL THEN 'D' 
@@ -25,24 +26,24 @@ SELECT TEAM_API_ID, COUNT(CASE WHEN home_team_goal < away_team_goal THEN 1 END) 
 FROM TEAM T JOIN MATCH M ON (T.TEAM_API_ID = M.AWAY_TEAM_API_ID)
 WHERE LEAGUE_ID = 1
 GROUP BY 1;
+*/
 
-
-SELECT L.TEAM_API_ID, 
-		SUM(L.PartidosPerdidos+V.PartidosPerdidos) AS "MATCHES WIN",
+SELECT L.TEAM_API_ID AS "TEAM ID", 
+		SUM(L.PartidosPerdidos+V.PartidosPerdidos) AS "MATCHES LOSE",
 		SUM(L.PartidosEmpatados+V.PartidosEmpatados) AS "MATCHES DRAW",
-		SUM(L.PartidosPerdidos+V.PartidosPerdidos) AS "MATCHES DRAW"
+		SUM(L.PartidosGanados+V.PartidosGanados) AS "MATCHES WIN"
 FROM (SELECT TEAM_API_ID, COUNT(CASE WHEN home_team_goal < away_team_goal THEN 1 END) AS PartidosPerdidos,
        COUNT(CASE WHEN home_team_goal = away_team_goal THEN 1 END) AS PartidosEmpatados,
        COUNT(CASE WHEN home_team_goal > away_team_goal THEN 1 END) AS PartidosGanados
 		FROM TEAM T JOIN MATCH M ON (T.TEAM_API_ID = M.HOME_TEAM_API_ID)
-		WHERE LEAGUE_ID = 1
+		WHERE LEAGUE_ID = 1 AND SEASON = '2008/2009'
 		GROUP BY 1) L 
 	JOIN
 		(SELECT TEAM_API_ID, COUNT(CASE WHEN home_team_goal < away_team_goal THEN 1 END) AS PartidosPerdidos,
        COUNT(CASE WHEN home_team_goal = away_team_goal THEN 1 END) AS PartidosEmpatados,
        COUNT(CASE WHEN home_team_goal > away_team_goal THEN 1 END) AS PartidosGanados
 		FROM TEAM T JOIN MATCH M ON (T.TEAM_API_ID = M.AWAY_TEAM_API_ID)
-		WHERE LEAGUE_ID = 1
+		WHERE LEAGUE_ID = 1 AND SEASON = '2008/2009'
 	GROUP BY 1) V ON L.TEAM_API_ID = V.TEAM_API_ID
 GROUP BY 1
 ORDER BY 4 DESC;

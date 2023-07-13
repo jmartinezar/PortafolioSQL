a45f44c2d381e7cc7ba69e32b2f964440d2b5b34
jmartinezar <jmartinezar@unal.edu.co>
2023-07-13 12:44:29 -0500

    se agrega query que genera una tabla con partidos ganados, empatados y perdidos en condición de visitante

--- SoccerDB/SoccerDataAnalysis.sql
+++ SoccerDB/SoccerDataAnalysis.sql
@@ -1,4 +1,4 @@
---Query inicial, busca en principio generar una tabla de posiciones estándar
+/*--Query inicial, busca en principio generar una tabla de posiciones estándar
 SELECT TEAM_API_ID, CASE 
 					WHEN HOME_TEAM_GOAL > AWAY_TEAM_GOAL THEN 'W' 
 					WHEN HOME_TEAM_GOAL = AWAY_TEAM_GOAL THEN 'D' 
@@ -7,3 +7,12 @@ FROM TEAM T JOIN MATCH M ON (TEAM_API_ID = HOME_TEAM_API_ID)
 WHERE LEAGUE_ID = 1
 GROUP BY 1
 ORDER BY 2 DESC;
+*/
+--Query que cuente los partidos ganados ganados, empatados y perdidos en condición de local por cada equipo
+
+SELECT TEAM_API_ID, COUNT(CASE WHEN home_team_goal < away_team_goal THEN 1 END) AS PartidosPerdidos,
+       COUNT(CASE WHEN home_team_goal = away_team_goal THEN 1 END) AS PartidosEmpatados,
+       COUNT(CASE WHEN home_team_goal > away_team_goal THEN 1 END) AS PartidosGanados
+FROM TEAM T JOIN MATCH M ON (T.TEAM_API_ID = M.HOME_TEAM_API_ID)
+WHERE LEAGUE_ID = 1
+GROUP BY 1;

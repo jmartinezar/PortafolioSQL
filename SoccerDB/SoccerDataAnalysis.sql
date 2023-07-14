7084f43a59f30c5b1416a1a8a007e1db0dd1b1e3
jmartinezar <jmartinezar@unal.edu.co>
2023-07-13 23:28:07 -0500

    se agrega un fragmento que crea una tabla de posiciones dentro de la base de datos y le agrega una columna correspondiente al total de partidos jugados por cada equipo esa temporada

--- SoccerDB/SoccerDataAnalysis.sql
+++ SoccerDB/SoccerDataAnalysis.sql
@@ -30,6 +30,7 @@ GROUP BY 1;
 
 -- Query que genera una tabla con los equipos de la liga con id 1 que hayan jugado la temporada 2008/2009, para cada equipo muestra la cantidad de partidos ganados, empatados y perdidos
 
+CREATE TABLE IF NOT EXISTS "Tabla de posiciones" AS
 SELECT L.TEAM_API_ID AS "TEAM ID", 
 		SUM(L.PartidosPerdidos+V.PartidosPerdidos) AS "MATCHES LOSE",
 		SUM(L.PartidosEmpatados+V.PartidosEmpatados) AS "MATCHES DRAW",
@@ -49,3 +50,9 @@ FROM (SELECT TEAM_API_ID, COUNT(CASE WHEN home_team_goal < away_team_goal THEN 1
 	GROUP BY 1) V ON L.TEAM_API_ID = V.TEAM_API_ID
 GROUP BY 1
 ORDER BY 4 DESC;
+
+ALTER TABLE "Tabla de posiciones" ADD COLUMN IF NOT EXISTS "Partidos jugados" INT;
+
+UPDATE "Tabla de posiciones" SET "Partidos jugados"= "MATCHES LOSE"+"MATCHES DRAW"+"MATCHES WIN";
+
+SELECT * FROM "Tabla de posiciones";
